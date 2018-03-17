7726ffe88212998d14a6b282d488e1e0bdf5fef8
270214174@qq.com <714251011@qq.com>
2018-03-17 12:37:04 +0800

    修改

--- Tab.js
+++ Tab.js
@@ -1,61 +1,60 @@
-       function Tab(obj){
-		   	
-		   	       this.tab=obj.tab
-		           this.box=obj.box
-		   	       this.tab.eq(0).addClass("hover")
-		   	       this.auto=obj.auto||false
-		   	       this.time=obj.time||1000
-		    		      
-		   		}      
+function Tab(obj){
+	
+		   this.tab=obj.tab
+		   this.box=obj.box
+		   this.tab.eq(0).addClass("hover")
+		   this.auto=obj.auto||false
+		   this.time=obj.time||1000
+				  
+		}      
+   
+   Tab.prototype.run=function(){
 		   
-		   Tab.prototype.run=function(index){
-		   	       
-		   	       var that=this
-		         this.tab.each(function(index){
-		           	
-			           $(this).click(function(){
-			            
-		           	     that.box.eq(index).show(500)
-    		                               .siblings()
-    		                               .hide(500)
-		   			
-		   			     that.tab.eq(index)
-    		                     .addClass("hover")
-    	                         .siblings()
-    	                         .removeClass("hover")
-		           	   
-		           })
-		           	
-		           	
-		        })
-		   		
-		   	
-		   		
-				if(this.auto){
-				   var j=0
-					setInterval(function(){
-						
-						    j++
-						if(j>that.tab.length){
-						   	
-						   	j=0
-						   	
-						   }
-					
-						  that.box.eq(j).show(500)
-    		                            .siblings()
-    		                            .hide(500)
-		   		        
-		   			     that.tab.eq(j)
-    		                     .addClass("hover")
-    	                         .siblings()
-    	                         .removeClass("hover")
-						
-						
-					},that.time)
-					
-					
-				}
+		 var that=this
+		 this.tab.each(function(index){
 			
-		   }
+			   $(this).click(function(){
+				
+				 that.box.eq(index).show(500)
+								   .siblings()
+								   .hide(500)
+			
+				 that.tab.eq(index)
+						 .addClass("hover")
+						 .siblings()
+						 .removeClass("hover")
+			   
+		   })
+			
+			
+		})
+		
+	
+	if(this.auto){
+	   var j=0
+		setInterval(function(){
+			
+				j++
+			if(j>that.tab.length){
+				
+				j=0
+				
+			   }
+		
+			  that.box.eq(j).show(500)
+							.siblings()
+							.hide(500)
+			
+			 that.tab.eq(j)
+					 .addClass("hover")
+					 .siblings()
+					 .removeClass("hover")
+			
+			
+		},that.time)
+		
+		
+	}
+	
+   }
 
